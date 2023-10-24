20d0e05cf67744ba6adf1bf2c4de2be9c166d32b
Teachbreathart <148863326+Teachbreathart@users.noreply.github.com>
2023-10-24 11:23:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function zoom(cell_id){
+    console.log(cell_id)
+    document.getElementById(cell_id).style.margin = "0px";
+
+}
+function shrink(cell_id){
+    document.getElementById(cell_id).style.margin = "10px";
+
+}
